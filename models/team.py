8cf7359bc5820f5408ecf63572b40dd0a21045d0
FawkesOficial <mario.lourenco.morte@gmail.com>
2024-06-12 23:21:15 +0100

    now using dataclasses for Team

--- models/team.py
+++ models/team.py
@@ -1,78 +1,73 @@
-from typing import List
-
-from models.player import Player
+from dataclasses import dataclass, field
 
 
 class PlayerAlreadyInTeam(Exception):
 
-    def __init__(self, player: Player, team_name: str):
-        super().__init__(f"player {player.name} is already in the team \"{team_name}\"")
+    def __init__(self, player_id: int, team_name: str):
+        super().__init__(f"player {player_id} is already in the team \"{team_name}\"")
 
 
 class PlayerNotInTeam(Exception):
 
-    def __init__(self, player: Player, team_name: str):
-        super().__init__(f"player {player.name} is not on the team \"{team_name}\"")
+    def __init__(self, player_id: int, team_name: str):
+        super().__init__(f"player {player_id} is not on the team \"{team_name}\"")
 
 
 class PlayerAlreadyIsOwner(Exception):
 
-    def __init__(self, player: Player, team_name: str):
-        super().__init__(f"player {player.name} already is the owner of the team \"{team_name}\"")
+    def __init__(self, player_id: int, team_name: str):
+        super().__init__(f"player {player_id} already is the owner of the team \"{team_name}\"")
 
 
+@dataclass(frozen=True)
 class Team:
+    name: str
+    owner_id: int
+    players: list[int] = field(default_factory=list)
+    points: int = 0
 
-    def __init__(self, name: str, owner: Player):
-        self.name: str = name
-        self.players: List[Player] = []
-        self.points: int = 0
-        self.owner: Player = owner
+    def __post_init__(self):
+        self.add_player(self.owner_id)
 
-        self.add_player(owner)
-
-    def add_player(self, player: Player) -> None:
+    def add_player(self, player_id: int) -> None:
         """
         Adds a player to the team
 
-        :param player: the player to add to the team
+        :param player_id: the player to add to the team
         :raises PlayerAlreadyInTeam: if the player is already in the team
         """
 
-        if player in self.players:
-            raise PlayerAlreadyInTeam(player, self.name)
+        if player_id in self.players:
+            raise PlayerAlreadyInTeam(player_id, self.name)
 
-        self.players.append(player)
+        self.players.append(player_id)
 
-    def remove_player(self, player: Player) -> None:
+    def remove_player(self, player_id: int) -> None:
         """
         Removes a player from the team
 
-        :param player: the player to remove from the team
+        :param player_id: the player to remove from the team
         :raises PlayerNotInTeam: if the player is not in the team
         """
 
-        if player not in self.players:
-            raise PlayerNotInTeam(player, self.name)
+        if player_id not in self.players:
+            raise PlayerNotInTeam(player_id, self.name)
 
-        self.players.remove(player)
+        self.players.remove(player_id)
 
-    def transfer_ownership(self, new_owner: Player) -> None:
+    def transfer_ownership(self, new_owner_id: int) -> None:
         """
         Removes a player from the team
 
-        :param new_owner: the player to transfer the ownership to
+        :param new_owner_id: the player to transfer the ownership to
         :raises PlayerNotInTeam: if the player is not in the team
         :raises PlayerAlreadyIsOwner: if new_owner already is the team's owner
         """
 
-        if new_owner not in self.players:
-            raise PlayerNotInTeam(new_owner, self.name)
+        if new_owner_id not in self.players:
+            raise PlayerNotInTeam(new_owner_id, self.name)
 
-        if self.owner == new_owner:
-            raise PlayerAlreadyIsOwner(new_owner, self.name)
+        if self.owner_id == new_owner_id:
+            raise PlayerAlreadyIsOwner(new_owner_id, self.name)
 
-        self.owner = new_owner
-
-    def __repr__(self) -> str:
-        return f"Team(name={self.name}, players={self.players}, points={self.points}, owner={self.owner})"
+        object.__setattr__(self, "owner_id", new_owner_id)
