78e43797673ee30d812ff0c7f1c0bf7e585fda0d
FawkesOficial <mario.lourenco.morte@gmail.com>
2024-06-14 20:17:38 +0100

    player -> player_id + docstring for the class itself

--- models/team.py
+++ models/team.py
@@ -21,8 +21,21 @@ class PlayerAlreadyIsOwner(Exception):
 
 @dataclass(frozen=True)
 class Team:
+    """
+    Represents a generic team of players.
+
+    Attributes
+    ========
+    name - the team's name
+    owner_id - the Discord ID of the team's owner
+    guild_id - the ID of the Discord Guild where the team was created
+    players - the list of the team's players Discord IDs
+    points - the number of points the team has
+    """
+
     name: str
     owner_id: int
+    guild_id: int
     players: list[int] = field(default_factory=list)
     points: int = 0
 
@@ -33,7 +46,7 @@ class Team:
         """
         Adds a player to the team
 
-        :param player_id: the player to add to the team
+        :param player_id: the Discord ID of the player to add to the team
         :raises PlayerAlreadyOnTheTeam: if the player is already on the team
         """
 
@@ -46,7 +59,7 @@ class Team:
         """
         Removes a player from the team
 
-        :param player_id: the player to remove from the team
+        :param player_id: the Discord ID of the player to remove from the team
         :raises PlayerNotOnTheTeam: if the player is not in the team
         """
 
@@ -59,7 +72,7 @@ class Team:
         """
         Removes a player from the team
 
-        :param new_owner_id: the player to transfer the ownership to
+        :param new_owner_id: the Discord ID of the player to transfer the ownership to
         :raises PlayerNotOnTheTeam: if the player is not in the team
         :raises PlayerAlreadyIsOwner: if new_owner already is the team's owner
         """
@@ -76,7 +89,7 @@ class Team:
         """
         Checks if the team is owned by the given player
 
-        :param player_id: the player to check
+        :param player_id: the Discord ID of the player to check
         :return: `True` if the player is the owner of the team, `False` otherwise
         """
         return self.owner_id == player_id
