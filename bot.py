927f09200179f706472d0ae076417f6200b1b680
FawkesOficial <mario.lourenco.morte@gmail.com>
2024-06-18 23:43:08 +0100

    better bot.py structure. more modular and can be improved in the future

--- bot.py
+++ bot.py
@@ -3,35 +3,38 @@ from discord.ext import commands
 
 import config
 
-client = commands.Bot(command_prefix=config.PREFIX, intents=config.INTENTS)
+
+class RaidedBot(commands.Bot):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def setup_hook(self) -> None:
+        await self.load_cogs()
+        await self.tree.sync()
+
+    async def load_cogs(self) -> None:
+        for cog_file in config.COGS_DIR.glob("*.py"):
+            if cog_file.name != "__init__.py":
+                cog: str = f"{config.COGS_DIR.relative_to(config.BASE_DIR)}.{cog_file.name.removesuffix('.py')}"
+
+                await self.load_extension(cog)
+
+                print(f"[+] Loaded {cog}")
+
+    async def on_ready(self):
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="/help"))
+
+        print(f"\n[+] Logged in as {self.user}\n")
+
+        print(f"Connected to {len(self.guilds)} server(s):")
+        for guild in self.guilds:
+            print(f"- {guild.name} ({guild.id}): {guild.member_count} members")
 
 
-async def load_cogs() -> None:
-    for cog_file in config.COGS_DIR.glob("*.py"):
-        if cog_file.name != "__init__.py":
-            cog: str = f"{config.COGS_DIR.relative_to(config.BASE_DIR)}.{cog_file.name.removesuffix('.py')}"
-
-            await client.load_extension(cog)
-
-            print(f"[+] Loaded {cog}")
-
-
-@client.event
-async def on_ready():
-    await load_cogs()
-
-    await client.tree.sync()
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="/help"))
-
-    print(f"\n[+] Logged in as {client.user}\n")
-
-    print(f"Connected to {len(client.guilds)} server(s):")
-    for guild in client.guilds:
-        print(f"- {guild.name} ({guild.id}): {guild.member_count} members")
-
-
-def main() -> None:
-    client.run(config.TOKEN)
+def main():
+    bot = RaidedBot(command_prefix=config.PREFIX, intents=config.INTENTS)
+    bot.run(config.TOKEN)
 
 
 if __name__ == "__main__":
